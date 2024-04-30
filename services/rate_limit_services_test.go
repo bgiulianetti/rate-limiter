29b25ff484129f5b8998866770fda8eda697c6f1
Bruno Giulianetti <bgiulianetti@amazon.com>
2024-04-30 15:05:03 -0300

    Comment not working test

--- services/rate_limit_services_test.go
+++ services/rate_limit_services_test.go
@@ -200,45 +200,68 @@ func TestRateLimitService_SendNotification_Success_WithinInterval_LimitNotExceed
 }
 
 // func TestRateLimitService_SendNotification_Success_IntervalElapsed(t *testing.T) {
-// 	mockNotificationsContainer := &NotificationsContainerMock{
-// 		AddNotificationFunc: func(userID string, notificationType string) error {
-// 			return nil
-// 		},
-// 		GetNotificationsByUserFunc: func(params domain.GetNotificationParams) ([]*domain.Notification, error) {
-// 			return []*domain.Notification{
-// 				{
-// 					Timestamp: time.Now().Add(-time.Second * 10),
-// 					UserID:    userIDTest,
-// 					Type:      notificationTypeTest,
-// 				},
-// 				{
-// 					Timestamp: time.Now().Add(-time.Second * 5),
-// 					UserID:    userIDTest,
-// 					Type:      notificationTypeTest,
-// 				},
-// 			}, nil
-// 		},
-// 	}
-// 	mockRulesContainer := &RulesContainerMock{
-// 		GetRuleByTypeFunc: func(s string) (*domain.RateLimitRule, error) {
-// 			return &domain.RateLimitRule{
-// 				NotificationType: "news",
-// 				MaxLimit:         2,
-// 				TimeInterval:     domain.Duration{Duration: time.Second * 10},
-// 			}, nil
-// 		},
-// 	}
+// 	// mockRulesContainer := &RulesContainerMock{
+// 	// 	GetRuleByTypeFunc: func(s string) ([]*domain.RateLimitRule, error) {
+// 	// 		return []*domain.RateLimitRule{
+// 	// 			{
+// 	// 				NotificationType: "news",
+// 	// 				MaxLimit:         2,
+// 	// 				TimeInterval:     domain.Duration{Duration: time.Second * 5},
+// 	// 			},
+// 	// 		}, nil
+// 	// 	},
+// 	// }
 
-// 	rateLimitService := NewRateLimitService(mockNotificationsContainer, NewRulesService(mockRulesContainer))
-// 	err := rateLimitService.SendNotification(domain.SendNotificationParams{
+// 	rateLimitService := NewRateLimitService(
+// 		notifications.NewInMemoryNotificationsContainer(),
+// 		NewRulesService(rules.NewInMemoryRulesContainer()),
+// 	)
+
+// 	params := domain.SendNotificationParams{
 // 		UserID:           "user1",
 // 		NotificationType: "email",
+// 	}
+
+// 	rateLimitService.SendNotification(params)
+// 	notifications, _ := rateLimitService.notificationsContainer.GetNotificationsByUser(domain.GetNotificationParams{
+// 		UserID:           params.UserID,
+// 		NotificationType: params.NotificationType,
+// 		TimeInterval:     time.Hour * 100,
 // 	})
+// 	fmt.Println("Notifications from 1st send", utils.SerializeObject(notifications))
+// 	rateLimitService.SendNotification(params)
+// 	notifications, _ = rateLimitService.notificationsContainer.GetNotificationsByUser(domain.GetNotificationParams{
+// 		UserID:           params.UserID,
+// 		NotificationType: params.NotificationType,
+// 		TimeInterval:     time.Hour * 100,
+// 	})
+// 	fmt.Println("Notifications from 2nd send", utils.SerializeObject(notifications))
+// 	err := rateLimitService.SendNotification(params)
+// 	notifications, _ = rateLimitService.notificationsContainer.GetNotificationsByUser(domain.GetNotificationParams{
+// 		UserID:           params.UserID,
+// 		NotificationType: params.NotificationType,
+// 		TimeInterval:     time.Hour * 100,
+// 	})
+// 	fmt.Println("Notifications from 3rd send", utils.SerializeObject(notifications))
+
+// 	//err := ExcecuteFuncNtimes(rateLimitService.SendNotification, params, 3)
+// 	fmt.Println("err frm send", utils.SerializeObject(err))
 // 	assert.Equal(t, errors.ErrRateLimitExceeded, err)
+
+// 	fmt.Println("Waiting  10 seconds for test ...")
 // 	time.Sleep(time.Second * 10)
+
 // 	err = rateLimitService.SendNotification(domain.SendNotificationParams{
 // 		UserID:           "user1",
 // 		NotificationType: "email",
 // 	})
 // 	assert.Equal(t, nil, err)
+// }
+
+// func ExcecuteFuncNtimes(fn func(domain.SendNotificationParams) error, params domain.SendNotificationParams, times int) error {
+// 	var err error
+// 	for i := 0; i < times; i++ {
+// 		err = fn(params)
+// 	}
+// 	return err
 // }
